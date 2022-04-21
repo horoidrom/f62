de48d7ad33f5513d27079795c6e435ac0eb39b0c
horoid <59495159+horoidrom@users.noreply.github.com>
2022-04-21 08:21:56 +0530

    new

---
+++ twrp_f62ins.mk
@@ -0,0 +1,14 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from f62 device
+$(call inherit-product, device/samsung/f62/device.mk)
+# Device identifier
+PRODUCT_DEVICE := f62ins
+PRODUCT_NAME := twrp_f62ins
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy F62
+PRODUCT_MANUFACTURER := samsung
